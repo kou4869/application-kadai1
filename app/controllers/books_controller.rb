05aca0479ebf5a3caa456f7ed62900e2810a9bab
EC2 Default User <ec2-user@ip-172-31-6-169.ap-northeast-1.compute.internal>
2023-01-11 11:55:06 +0000

    2回目のコミット

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,29 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    flash[:notice] = "ブックが正常に作成されました。"
+    redirect_to "/books/#{@book.id}"
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
